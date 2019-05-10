b63bb36ea5b74d89c42d9b649bad3d8a32e43d80
m860 <jean.ma.1986@gmail.com>
2019-05-10 11:47:02 +0800

    refactor: 优化字体缩放的处理

--- src/index.js
+++ src/index.js
@@ -12,19 +12,21 @@ export default function (React) {
             width: props.width,
             height: props.height
         });
+        let children = props.children;
         if ("fontSize" in style) {
             style.fontSize = style.fontSize * props.scale;
-            //处理chrome字体小于12的问题
-            //TODO 处理transform-origin的问题
+            //处理chrome字体小于12的问题，此时只处理字体元素
             if (style.fontSize < 12) {
-                if ("transform" in style) {
-                    style.transform += `;scale(${style.fontSize / 12})`;
-                } else {
-                    style.transform = `scale(${style.fontSize / 12})`;
-                }
+                const textStyle = {
+                    fontSize: 12,
+                    transform: `scale(${style.fontSize / 12})`
+                };
+                children = (
+                    <div style={textStyle}>{props.children}</div>
+                );
             }
         }
-        return <div style={style}>{props.children}</div>
+        return <div style={style}>{children}</div>
     }
 
     Text.defaultProps = {
